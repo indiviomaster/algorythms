0bc984c47024329fca22b762d679f247cc9cbfb9
indivioM <indivio@yandex.ru>
2021-02-16 18:08:04 +0300

    Add function pow

---
+++ src/ru/indivio/algorithms/RecPow.java
@@ -0,0 +1,30 @@
+package ru.indivio.algorithms;
+
+public class RecPow {
+    public static void main(String[] args) {
+        System.out.println(pow(10.0,-10));
+    }
+    public static double pow(double number, int power){
+        if(power == 0){
+            return 1;
+        }
+        if(power == 1){
+            return number;
+        }
+        if(power == -1){
+            return 1/number;
+        }
+        if (power < 0) {
+            return powNum(1/number,1/number,-power);
+        }
+        return powNum(number, number, power);
+    }
+
+    private static double powNum(double acc, double n, int p) {
+        if (p == 1) {
+            return acc;
+        }
+
+        return powNum(acc*n, n, p-1);
+    }
+}
